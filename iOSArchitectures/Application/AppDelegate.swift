49a9d62940da4e03f097c5b8056ac14f7b5649f2
Robert Gravina <robert.gravina@gmail.com>
2018-08-24 13:32:27 +0900

    Created new app. Removed some generated code.

---
+++ iOSArchitectures/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow()
+        if let window = window {
+            window.rootViewController = ViewController()
+            window.makeKeyAndVisible()
+        }
+        return true
+    }
+}
+
