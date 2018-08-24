39817637b32860a3206e3fc875f693ed65b6b27a
Robert Gravina <robert.gravina@gmail.com>
2018-08-24 13:22:05 +0900

    Initial Commit

---
+++ iOSArchitectures/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iOSArchitectures
+//
+//  Created by Robert Gravina on 2018/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
