57c88f197287686c2ad375155a8cd11e0f02dfd4
Robert Gravina <robert.gravina@gmail.com>
2018-09-30 18:07:49 +0900

    Add table view cells for other architectures.

--- iOSArchitectures/ArchitectureListViewController.swift
+++ iOSArchitectures/ArchitectureListViewController.swift
@@ -13,7 +13,7 @@ class ArchitectureListViewController: UIViewController, UITableViewDelegate, UIT
         let margins = view.layoutMarginsGuide
 
         view.backgroundColor = UIColor.black
-        tableView.backgroundColor = UIColor.black
+        tableView.backgroundColor = UIColor.black.lighter(by: 8.0)
         tableView.separatorColor = UIColor.lightGray
         tableView.delegate = self
         tableView.dataSource = self
@@ -29,14 +29,28 @@ class ArchitectureListViewController: UIViewController, UITableViewDelegate, UIT
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 4
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Model View Controller"
-        cell.textLabel?.textColor = UIColor.white
         cell.backgroundColor = UIColor.black.lighter(by: 12.0)!
+        cell.textLabel?.textColor = UIColor.white
+        cell.selectedBackgroundView = UIView()
+        cell.selectedBackgroundView?.backgroundColor = UIColor.black.lighter(by: 16.0)!
+
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "Model View Controller (MVC)"
+        case 1:
+            cell.textLabel?.text = "MVC With Improvements"
+        case 2:
+            cell.textLabel?.text = "Model-View-ViewModel (MVVM)"
+        case 3:
+            cell.textLabel?.text = "VIPER (Clean Architecture)"
+        default:
+            break;
+        }
         return cell
     }
 }
