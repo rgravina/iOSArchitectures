c334c01a6ec957a35a647c079254b2eba60e91b9
Robert Gravina <robert.gravina@gmail.com>
2018-10-08 21:43:11 +0900

    Update text when button is tapped.

---
+++ iOSArchitectures/ButtonColor.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension Notification.Name {
+    static let tapped = NSNotification.Name(rawValue: "tapped")
+}
+
+enum ButtonColor: Int {
+    case red, green, blue
+}
--- iOSArchitectures/ModelViewControllerViewController.swift
+++ iOSArchitectures/ModelViewControllerViewController.swift
@@ -6,6 +6,7 @@ class ModelViewControllerViewController: UIViewController {
     let blueButton = UIButton()
     let label = UILabel()
     let stackView = UIStackView()
+    var lastTapped: ButtonColor?
 
     override func viewDidLoad() {
         setupView()
@@ -21,20 +22,27 @@ class ModelViewControllerViewController: UIViewController {
     }
 
     private func setupSubviews() {
-        redButton.translatesAutoresizingMaskIntoConstraints = false
         redButton.setTitle("Red", for: .normal)
         redButton.setTitleColor(Theme.listBackgroundColor, for: .normal)
         redButton.backgroundColor = Theme.red
+        redButton.addTarget(self, action: #selector(tappedRed), for: .touchUpInside)
 
-        blueButton.translatesAutoresizingMaskIntoConstraints = false
         blueButton.setTitle("Blue", for: .normal)
         blueButton.setTitleColor(Theme.listBackgroundColor, for: .normal)
         blueButton.backgroundColor = Theme.secondaryColor
+        blueButton.addTarget(self, action: #selector(tappedBlue), for: .touchUpInside)
 
-        greenButton.translatesAutoresizingMaskIntoConstraints = false
         greenButton.setTitle("Green", for: .normal)
         greenButton.setTitleColor(Theme.listBackgroundColor, for: .normal)
         greenButton.backgroundColor = Theme.primaryColor
+        greenButton.addTarget(self, action: #selector(tappedGreen), for: .touchUpInside)
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(updatedModel),
+            name: .tapped,
+            object: lastTapped
+        )
 
         label.text = "Press a button"
         label.textAlignment = .center
@@ -63,5 +71,38 @@ class ModelViewControllerViewController: UIViewController {
             stackView.rightAnchor.constraint(equalTo: margins.rightAnchor),
             stackView.bottomAnchor.constraint(equalTo: margins.bottomAnchor)
         ])
+    }
+
+    @objc func tappedRed() {
+        lastTapped = .red
+        NotificationCenter.default.post(name: .tapped, object: nil, userInfo: nil)
+    }
+
+    @objc func tappedGreen() {
+        lastTapped = .green
+        NotificationCenter.default.post(name: .tapped, object: nil, userInfo: nil)
+    }
+
+    @objc func tappedBlue() {
+        lastTapped = .blue
+        NotificationCenter.default.post(name: .tapped, object: nil, userInfo: nil)
+    }
+
+    @objc func updatedModel() {
+        if (lastTapped == nil) {
+            label.text = "Press a button"
+        } else {
+            switch (lastTapped!) {
+            case .red:
+                label.text = "Red was pressed"
+                break
+            case .green:
+                label.text = "Green was pressed"
+                break
+            case .blue:
+                label.text = "Blue was pressed"
+                break
+            }
+        }
     }
 }
